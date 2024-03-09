84f65827fe3eb4597fd07502560eadaa55ba9703
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-09 23:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function updateTime() {
+  let timer = document.getElementById('timer');
+  let now = new Date();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  let year = now.getFullYear();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  if (hours > 12) {
+    hours = hours - 12;
+    timer.textContent = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} PM`;
+  } else if (hours === 0) {
+    hours = 12
+    timer.textContent = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} AM`;
+  } else {
+    timer.textContent = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} AM`;
+  }
+}
+  // Call updateTime every second
+  setInterval(updateTime, 1000);
